e5b82516eb2947fe5f064c480f8947d7e5611b43
scribu <mail@scribu.net>
2009-06-16 11:42:15 +0000

    git-svn-id: http://plugins.svn.wordpress.org/scb-framework/trunk@126409 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/BoxesPage.php
+++ classes/BoxesPage.php
@@ -45,6 +45,7 @@ abstract class scbBoxesPage extends scbAdminPage
 .inside {clear:both; overflow:hidden; padding: 10px 10px 0 10px !important}
 .inside table {margin: 0 !important; padding: 0 !important}
 .inside table td {vertical-align: middle !important}
+.inside table .regular-text {width: 100% !important}
 .inside .form-table th {width: 30%; max-width: 200px; padding: 10px 0 !important}
 .inside .widefat .check-column {padding-bottom: 7px !important}
 .inside p, .inside table {margin: 0 0 10px 0 !important}
