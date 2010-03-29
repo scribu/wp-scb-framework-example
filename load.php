5616f6ef3bdaf15efc5c97159b9b58eacccf22fb
scribu <mail@scribu.net>
2010-03-29 03:23:27 +0000

    first pass at scbLoad3
    git-svn-id: http://plugins.svn.wordpress.org/scb-framework/trunk@222831 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- load.php
+++ load.php
@@ -1,6 +1,6 @@
 <?php
 /*
-You can use this code to autoload the available classes of the scbFramework.
+You can use this code to load the most recent versions of scbFramework available.
 This has the advantage that the user is not required to install scbFramework as a separate plugin.
 Things to note:
 - you have to include the class files with each plugin
@@ -13,37 +13,58 @@ To load the classes, you just need to require the file, like so:
 This file needs to be in the same directory as the class files.
 */
 
-if ( !class_exists('scbLoad2') ) :
-abstract class scbLoad2 {
-	private static $path;
+if ( !class_exists('scbLoad3') ) :
+abstract class scbLoad3 {
 
-	static function init($file, $classes) {
-		self::$path = dirname($file) . '/';
+	private static $candidates;
+	private static $loaded = false;
 
-		if ( class_exists('scbFramework') )
-			self::$path .= 'classes/';
-
-		foreach ( $classes as $className )
-			self::load($className);
+	static function register($rev, $file, $classes) {
+		if ( class_exists('scbFramework') ) {
+			self::load(dirname($file) . '/classes/', $classes);
+		}
+		else {
+			self::$candidates[$rev] = array($file, $classes);
+			add_action('plugins_loaded', array(__CLASS__, 'init'), 0);
+		}
 	}
 
-	private static function load($className) {
-		if ( class_exists($className) )
-			return false;
+	static function init() {
+		krsort(self::$candidates);
 
-		$fpath = self::$path . substr($className, 3) . '.php';
+		list($file, $classes) = reset(self::$candidates);
 
-		if ( ! @file_exists($fpath) )
-			return false;
+		self::load(dirname($file) . '/', $classes);
+	}
 
-		include $fpath;
+	private static function load($path, $classes) {
+		if ( self::$loaded )
+			return;
+		self::$loaded = true;
 
-		return true;
+		foreach ( $classes as $class_name ) {
+			if ( class_exists($class_name) )
+				continue;
+
+			$fpath = $path . substr($class_name, 3) . '.php';
+
+			if ( @file_exists($fpath) )
+				include $fpath;
+		}
+	}
+
+	static function delayed_activation($plugin) {
+		register_activation_hook($plugin, array(__CLASS__, '_delayed_activation'));
+	}
+
+	static function _delayed_activation() {
+		do_action('plugins_loaded');
+		do_action(current_filter());
 	}
 }
 endif;
 
-scbLoad2::init(__FILE__, array(
+scbLoad3::register('15', __FILE__, array(
 	'scbOptions', 'scbForms', 'scbAdminPage', 'scbBoxesPage',
 	'scbWidget', 'scbCron', 'scbTable', 'scbUtil', 'scbRewrite',
 ));
