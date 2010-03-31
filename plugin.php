f5f45614388c52666454d5ae3c6d9130ba93101a
scribu <mail@scribu.net>
2010-03-31 23:42:44 +0000

    introduce scbLoad3
    git-svn-id: http://plugins.svn.wordpress.org/scb-framework/trunk@223809 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -8,34 +8,12 @@ Author URI: http://scribu.net
 Plugin URI: http://scribu.net/wordpress/scb-framework
 */
 
-abstract class scbFramework {
-	const version = '1.4';
+require dirname(__FILE__) . '/scb/load.php';
 
-	static function init() {
-		require_once dirname(__FILE__) . '/load.php';
-
-		add_action('shutdown', array(__CLASS__, 'put_first'));
+function _scb_test() {
+	function dummy() {
+		// echo 'hi';
 	}
-
-	static function put_first() {
-		$plugin = plugin_basename(__FILE__);
-		$current = get_option('active_plugins');
-
-		if ( $current[0] == $plugin )
-			return;		// plugin already first
-
-		$i = array_search($plugin, $current);
-
-		if ( FALSE === $i )
-			return;		// plugin not active
-
-		// Move current plugin to the first position
-		array_splice($current, $i, 1);
-		array_unshift($current, $plugin);
-
-		update_option('active_plugins', $current);
-	}
+	dummy();
 }
-
-scbFramework::init();
-
+new scbLoad3(__FILE__, '_scb_test');
