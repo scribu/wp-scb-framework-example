6a468325de00d6000e43acc564777405003a7ee5
scribu <mail@scribu.net>
2010-04-01 00:08:13 +0000

    git-svn-id: http://plugins.svn.wordpress.org/scb-framework/trunk@223824 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -10,10 +10,5 @@ Plugin URI: http://scribu.net/wordpress/scb-framework
 
 require dirname(__FILE__) . '/scb/load.php';
 
-function _scb_test() {
-	function dummy() {
-		// echo 'hi';
-	}
-	dummy();
-}
-new scbLoad3(__FILE__, '_scb_test');
+// Rest of your plugin code goes here
+
