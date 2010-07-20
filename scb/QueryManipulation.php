f3291b264175948a721cb9bd5779907e7bfa034d
scribu <mail@scribu.net>
2010-07-20 00:06:55 +0000

    introduce enable() & disable()
    git-svn-id: http://plugins.svn.wordpress.org/scb-framework/trunk@264706 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scb/QueryManipulation.php
+++ scb/QueryManipulation.php
@@ -5,26 +5,51 @@ class scbQueryManipulation {
 	private $bits = array();
 	private $wp_query;
 
-	function __construct( $callback ) {
+	private static $filters = array(
+		'posts_where',
+		'posts_join',
+		'posts_groupby',
+		'posts_orderby',
+		'posts_distinct',
+		'post_limits',
+		'posts_fields'
+	);
 
+	public function __construct( $callback, $once = true ) {
 		$this->callback = $callback;
 
-		$filters = array(
-			'posts_where',
-			'posts_join',
-			'posts_groupby',
-			'posts_orderby',
-			'posts_distinct',
-			'post_limits',
-			'posts_fields'
-		);
+		$this->enable();
+		
+		if ( !$once )
+			return;
 
-		foreach ( $filters as $filter ) {
+		add_filter( 'posts_request', array( $this, '_disable' ) );
+	}
+
+	function _disable( $request ) {
+		remove_filter( 'posts_request', array( $this, '_disable' ) );
+
+		$this->disable();
+
+		return $request;
+	}
+
+	public function enable() {
+		foreach ( self::$filters as $filter ) {
 			add_filter( $filter, array( $this, 'collect' ), 999, 2 );
 			add_filter( $filter . '_request' , array( $this, 'update' ), 9 );
 		}
 
 		add_action( 'posts_selection' , array( $this, 'alter' ) );
+	}
+
+	public function disable() {
+		foreach ( self::$filters as $filter ) {
+			remove_filter( $filter, array( $this, 'collect' ), 999, 2 );
+			remove_filter( $filter . '_request' , array( $this, 'update' ), 9 );
+		}
+
+		remove_action( 'posts_selection' , array( $this, 'alter' ) );
 	}
 
 	function collect( $value, $wp_query ) {
