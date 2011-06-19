92b3d4339c6158f1cba2a8ca563eb4a891a39d09
scribu <mail@scribu.net>
2011-06-19 13:59:08 +0000

    sync from github
    git-svn-id: http://plugins.svn.wordpress.org/scb-framework/trunk@398894 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scb/Util.php
+++ scb/Util.php
@@ -65,27 +65,14 @@ class scbUtil {
 
 	// Extract certain $keys from $array
 	static function array_extract( $array, $keys ) {
-		$r = array();
-
-		foreach ( $keys as $key )
-			if ( array_key_exists( $key, $array ) )
-				$r[$key] = $array[$key];
-
-		return $r;
+		_deprecated_function( 'scbUtil::array_extract', '3.1', 'wp_array_slice_assoc' );
+		return wp_array_slice_assoc( $array, $keys );
 	}
 
 	// Extract a certain value from a list of arrays
 	static function array_pluck( $array, $key ) {
-		$r = array();
-
-		foreach ( $array as $value ) {
-			if ( is_object( $value ) )
-				$value = get_object_vars( $value );
-			if ( array_key_exists( $key, $value ) )
-				$r[] = $value[$key];
-		}
-
-		return $r;
+		_deprecated_function( 'scbUtil::array_pluck', '3.1', 'wp_list_pluck' );
+		return wp_list_pluck( $array, $key );
 	}
 
 	// Transform a list of objects into an associative array
